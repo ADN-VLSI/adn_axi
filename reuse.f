80f8b5ba9bd93f4106856a32b6c52d5d08f5a75f
Foez Ahmed <foez.official@gmail.com>
2026-08-02 22:17:37 +0600

    auto-update

--- reuse.f
+++ reuse.f
@@ -1,4 +1,4 @@
-${ADN_ENDEC}/interface/dummy_interface.sv
-${ADN_ENDEC}/source/dummy_rtl.sv
+${ADN_AXI}/interface/dummy_interface.sv
+${ADN_AXI}/source/dummy_rtl.sv
+-i ${ADN_AXI}/include
 -i ${ADN_COMMON}/include
--i ${ADN_ENDEC}/include
